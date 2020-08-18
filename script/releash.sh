c4407d26366fa89f9aa5e49b59372153ab066490
刘泽 <1275839779@qq.com>
2020-08-18 14:59:39 +0800

    [build] 1.0.0

---
+++ script/releash.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -e
+echo "Enter release version: "
+read VERSION
+
+read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo "Releasing $VERSION ..."
+  
+  # build
+  VERSION=$VERSION npm run build
+
+  cp -rf dist docs
+
+  # commit
+  git add -A
+  git commit -m "[build] $VERSION"
+
+  # publish
+  git push origin refs/tags/v$VERSION
+  git push
+fi
