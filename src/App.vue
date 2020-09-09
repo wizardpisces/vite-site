3422ab3930cbb6308cfe8ed06d55c8adf6f363e9
刘泽 <1275839779@qq.com>
2020-09-09 18:45:43 +0800

    [build] 0.0.1

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,29 @@
 <template>
   <Menu msg="Hello Vue 3.0 + Vites" />
+  {{compiledResult}}
 </template>
 
 <script>
 import Menu from './components/menu.vue'
+import {compile} from  'tiny-sass-compiler/dist/tiny-sass-compiler.esm-browser.prod.js'
+
+const compiledResult = compile(`$font-stack:    Helvetica, sans-serif;
+$primary-color: #333;
+
+body .test{
+  font: 100% $font-stack;
+  color: 
+  $primary-color;
+}
+`)
 
 export default {
   name: 'App',
+  data(){
+    return {
+      compiledResult
+    };
+  },
   components: {
     Menu
   }
---
+++ src/test.ts
@@ -0,0 +1,8 @@
+const a = true;
+const test = 'test1'
+const test2 = 'test2'
+
+
+export {
+    test
+}
