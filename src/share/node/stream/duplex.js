2551872fc65ac2e990cc3498da9b8251d721e204
刘泽 <1275839779@qq.com>
2021-02-19 19:05:21 +0800

    chore: add node and interview share

---
+++ src/share/node/stream/duplex.js
@@ -0,0 +1,20 @@
+const {
+    Duplex
+} = require('stream');
+
+const inoutStream = new Duplex({
+    write(chunk, encoding, callback) {
+        console.log(chunk.toString());
+        callback();
+    },
+
+    read(size) {
+        this.push(String.fromCharCode(this.currentCharCode++));
+        if (this.currentCharCode > 90) {
+            this.push(null);
+        }
+    }
+});
+
+inoutStream.currentCharCode = 65;
+process.stdin.pipe(inoutStream).pipe(process.stdout);
---
+++ src/share/node/stream/readme.md
@@ -0,0 +1,4 @@
+## Node Stream
+
+## Reference
+* https://www.freecodecamp.org/news/node-js-streams-everything-you-need-to-know-c9141306be93/
