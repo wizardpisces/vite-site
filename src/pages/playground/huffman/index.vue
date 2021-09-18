5fae239bfd1526dab6e9a13b4158bcf397e8e5fb
刘泽 <1275839779@qq.com>
2021-09-18 11:53:14 +0800

    chore: change huffman-ts reference

--- src/pages/playground/huffman/index.vue
+++ src/pages/playground/huffman/index.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="huffman">
-        <h1>huffman demo</h1>
+        <h1>Demo of <a href="https://github.com/wizardpisces/huffman-ts" target="__blank">huffman-ts</a></h1>
         <ul>
-            <li>raw data: <input type="text" @input="onRawDataChange" v-model="huffmanData.rawString"></li>
-            <li>encoded bit string: "{{huffmanData.encodedBit}}"</li>
-            <li>encoded char string: "{{huffmanData.encodedCharString}}"</li>
-            <li>encoded huffman tree: "{{huffmanData.treeEncoded}}"</li>
+            <li>Raw data: <input type="text" @input="onRawDataChange" v-model="huffmanData.rawString"></li>
+            <li>Encoded bit string: "{{huffmanData.encodedBit}}"</li>
+            <li>Encoded char string: "{{huffmanData.encodedCharString}}"</li>
+            <li>Encoded huffman tree: "{{huffmanData.treeEncoded}}"</li>
         </ul>
     </div>
 
--- todo.md
+++ todo.md
@@ -1,4 +1,5 @@
 * ~~remove codemirror dep from global~~
+* Blog时间build自动生成
 * 站内搜索
 * add serviceWorker (fiddle with google server?)
 * 
