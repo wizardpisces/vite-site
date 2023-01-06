0ff17f40503bb1e86b4fa8f158887e052c727ef9
wizardpisces <1275839779@qq.com>
2023-01-06 14:44:10 +0800

    test: check different ref does not affect eachother

--- src/pages/test/toggle-translate-key.vue
+++ src/pages/test/toggle-translate-key.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="toggle-translate-key">
-        <button @click="toggleTranslateKey">{{t('toggleTranslateKey')}}</button>
+    <v3-button size="small" @click="toggleTranslateKey">{{
+      t("toggleTranslateKey")
+    }}</v3-button>
+    <v3-button size="small" @click="add"
+      >{{ t("add count") }} {{ count }}</v3-button
+    >
   </div>
 </template>
 <script lang="ts">
-import { defineComponent, inject } from "vue";
+import { defineComponent, inject, ref } from "vue";
 import { useI18N } from "../../utils/i18n";
 import { key, useStore } from "../../store";
 import { Mutations } from "../../store/type";
@@ -12,11 +17,16 @@ import { Mutations } from "../../store/type";
 export default defineComponent({
   setup() {
     // const store = useStore()
-    const store = inject(key)!
-    const {$t} = useI18N()
+    const store = inject(key)!;
+    const { $t } = useI18N();
+
+    let count = ref(0);
+    const add = () => count.value++;
     return {
-        toggleTranslateKey:()=>store.commit(Mutations.TOGGLE_TRANSLATE_KEY),
-        t:$t
+      toggleTranslateKey: () => store.commit(Mutations.TOGGLE_TRANSLATE_KEY),
+      t: $t,
+      count,
+      add,
     };
   },
 });
