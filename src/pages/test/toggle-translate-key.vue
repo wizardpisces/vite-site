c36047c1f4961f6433e1822b4e07b2f39f2272d9
wizardpisces <1275839779@qq.com>
2023-01-06 11:54:41 +0800

    test: add inject store

--- src/pages/test/toggle-translate-key.vue
+++ src/pages/test/toggle-translate-key.vue
@@ -4,14 +4,15 @@
   </div>
 </template>
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, inject } from "vue";
 import { useI18N } from "../../utils/i18n";
-import { useStore } from "../../store";
+import { key, useStore } from "../../store";
 import { Mutations } from "../../store/type";
 
 export default defineComponent({
   setup() {
-    const store = useStore()
+    // const store = useStore()
+    const store = inject(key)!
     const {$t} = useI18N()
     return {
         toggleTranslateKey:()=>store.commit(Mutations.TOGGLE_TRANSLATE_KEY),
